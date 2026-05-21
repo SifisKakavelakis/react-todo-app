d964db7b3197a558568cfe79042d1a4855f0a146
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-05-21 21:30:40 +0300

    feat(ui): add Header, Footer and Layout shared components

---
+++ src/shared/layout/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <>
+            <header className="bg-cf-dark-red fixed w-full">
+                <div className="container mx-auto px-4 flex items-center justify-between">
+                    <a href="/public">
+                        <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
+                             alt="cf-logo" className="my-4 h-16"/>
+                    </a>
+
+                    <nav className="flex gap-4 text-white font-medium">
+                        <a href="/public">Todo App</a>
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
