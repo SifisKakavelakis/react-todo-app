a3eaeeeee34f374322b16ec0b24be135ca1c979d
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-05-22 22:01:57 +0300

    feat(ui): add IconButton shared component

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,11 +1,18 @@
 export interface LayoutProps {
-    children?: React.ReactNode;
+    children: React.ReactNode;
     addClasses?: string;
 }
 
 export type ButtonProps = {
     onClick?: () => void;
     disabled?: boolean;
-    label?: string;
+    label: string;
+    addClasses?: string;
+}
+
+export type IconButtonProps = {
+    onClick?: () => void;
+    disabled?: boolean;
+    icon: React.ReactNode;
     addClasses?: string;
 }
